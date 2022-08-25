e1efeb840a378e3f7b254293941a065135c018d4
abayomi igwubor <unbeatable-abayomi>
2022-08-25 02:49:37 +0100

    implemented filterImageFromURL deleteLocalFiles helper functions

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -11,7 +11,52 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   const port = process.env.PORT || 8082;
   
   // Use the body parser middleware for post requests
-  app.use(bodyParser.json());
+  //app.use(bodyParser.json());
+
+  app.get("/filteredimage/", async ( req: Request, res: Response ) => {
+    try {
+      var { my_image_url_req } = req.query;
+      if (!my_image_url_req) {
+        return res.status(400)
+          .send('URL FOR AN IMAGE IS NEEDED')
+      }
+      var imageUrlTostring = my_image_url_req.toString()
+      const my_image_filtered = await filterImageFromURL(imageUrlTostring)
+      res.status(200)
+        .sendFile(my_image_filtered)
+      res.on('finish', () => deleteLocalFiles([my_image_filtered]));
+    } catch (error) {
+      return res.status(500)
+        .send('Image Could not Download Try Again')
+    }
+  });
+
+  // app.get("/filteredimage/", async ( req: Request, res: Response ) => {
+  //   try {
+  //     var { my_image_url_req } = req.query;
+  //     if (!my_image_url_req) {
+  //       return res.status(400)
+  //         .send('URL FOR AN IMAGE IS NEEDED')
+  //     }
+  //     var imageUrlTostring = my_image_url_req.toString()
+  //     const my_image_filtered = await filterImageFromURL(imageUrlTostring)
+  //     res.sendFile(my_image_filtered, options, function (err) {
+  //       if (err) {
+  //           next(err);
+  //       } else {
+  //           console.log('Sent:', my_image_filtered);
+  //           next();
+  //       }
+  //   });
+  //   });
+  //   } catch (error) {
+  //     return res.status(500)
+  //       .send('Image Could not Download Try Again')
+  //   }
+  // });
+
+
+
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
